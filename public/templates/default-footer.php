138e0f81d99ee6707f078db6efc4d455f1072b94
Shohei Sasamoto <shohei.sasamoto@gmail.com>
2013-10-06 15:22:16 +0900

    Add google analytics code

--- public/templates/default-footer.php
+++ public/templates/default-footer.php
@@ -6,5 +6,14 @@
     <script src="../bower_components/jquery/jquery.min.js"></script>
     <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="scripts/main.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-27086774-10', 'sandbox.in.net');
+      ga('send', 'pageview');
+    </script>
 </body>
 </html>
